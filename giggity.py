7ecf7d1502ef7d960cd5028639185fbd3c156a98
Adam Musciano <amusciano@gmail.com>
2019-01-18 03:27:35 -0500

    added followers flag

--- giggity.py
+++ giggity.py
@@ -4,14 +4,14 @@ import requests
 import json
 
 class giggity():
-
+    
     def __init__(self, auth_usr="", auth_pss=""):
 
         self.orgTree = {}
         self.header ={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
         self.auth = (auth_usr,auth_pss)
         
-    def getUsers(self, org, verbose=False):
+    def getUsers(self, org, verbose=False, followers=False):
         if(verbose):
             print("Sraping organization: "+org)
 
@@ -20,11 +20,12 @@ class giggity():
         tree = {}
 
         while(not isEmpty):
-            r = requests.get("https://api.github.com/orgs/"+org+"/members?page="+str(page)+"&per_page=10", headers=self.header, auth=self.auth)
+            r = requests.get("https://api.github.com/orgs/"+org+"/members?page="+str(page)+"&per_page=100", headers=self.header, auth=self.auth)
             result= r.json()
             page+=1
             if(len(result)==0):
                 isEmpty=True
+
             elif("message" in  result):
                 print("Organization not found")
             else:
@@ -32,6 +33,9 @@ class giggity():
                     #This is where you would branch out and do additional searches on an account
                     account["repos"]= self.getRepos(account["login"], verbose) #ie, get all user repos, then tie them back into the data structure before appending it to tree
 
+                    if(followers):
+                        account["followers"]= self.getFollowers(account["login"],verbose)
+                    
                     tree[account["login"]] = account
 
                 self.orgTree["users"] = tree
@@ -46,8 +50,7 @@ class giggity():
             print("Getting followers of user: "+user)
         page=1
         isEmpty=False;
-        followerList=[]
-        
+        tree= {}
         while(not isEmpty):
             r = requests.get("https://api.github.com/users/"+user+"/followers?page="+str(page)+"&per_page=100", headers=self.header, auth=self.auth)
             result= r.json()
@@ -57,16 +60,16 @@ class giggity():
             elif("message" in  result):
                 print("User not found")
             else:
-                if(verbose):
-                    print("Number of followers: "+str(len(result)))
-                followerList= followerList + result
+                for account in result:
+                    tree[account["login"]]= account
         
-        return followerList
+        return tree
 
 
     def getRepos(self, user, verbose=False):
         if(verbose):
             print("Getting repositories for user: "+user)
+
         page=1
         isEmpty=False;
         repoTree= {}
@@ -75,6 +78,7 @@ class giggity():
             r = requests.get("https://api.github.com/users/"+user+"/repos?page="+str(page)+"&per_page=100", headers=self.header, auth=self.auth)
             result = r.json()
             page+=1
+            
             if("message" in result):
                 print("User Not Found")
             elif(len(result)==0):
@@ -83,8 +87,6 @@ class giggity():
                 if(verbose):
                     print("Number of repositories: "+str(len(result)))
                 for repo in result:
-                    if(verbose):
-                        print("Getting repo: "+repo["name"])
                     tree= {"name": repo["name"],
                            "url":repo["html_url"],
                            "fork":repo["fork"],
@@ -122,9 +124,10 @@ if __name__ == '__main__':
     parser.add_argument("-a", "--authenticate", help="allows github authentication to avoid ratelimiting",action="store_true")
     parser.add_argument("-u", "--user",help="denotes that given input is a user",action="store_true")
     parser.add_argument("-o", "--org",help="denotes that given input is an organization",action="store_true")
+    parser.add_argument("-f", "--followers",help="adds followers entry to each account",action="store_true")
     parser.add_argument("-O", "--outfile", dest="output", help="location to put generated json file")
     parser.add_argument("path",help="name of organization or user (or url of  repository)")
-
+    
     
     
     args = parser.parse_args() 
@@ -147,13 +150,18 @@ if __name__ == '__main__':
         g = giggity()
 
     if(args.user):
-        res= g.getRepos(target, args.verbose)
+        tree={} 
+        tree["repos"]= g.getRepos(target, args.verbose)
+
+        if(args.followers):
+            tree["followers"] = g.getFollowers(target, args.verbose)
+
         with open(outfile , 'w') as out:
-            json.dump(res, out, indent=4 , sort_keys=True)
+            json.dump(tree, out, indent=4 , sort_keys=True)
 
 
     if(args.org):
-        g.getUsers(target, args.verbose)
+        g.getUsers(target, args.verbose, args.followers)
         g.writeToFile(outfile)
        
     print("Scraping Complete, file available at: "+outfile)
