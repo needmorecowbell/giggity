f76832b16c7a7f76542bb1da88b108359c8d7c34
Adam Musciano <amusciano@gmail.com>
2020-02-17 10:01:29 -0500

    new release on pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="giggity", 
-    version="1.0.0",
+    version="1.0.1",
     author="Adam Musciano",
     author_email="amusciano@gmail.com",
     description="CLI Git Sleuth Tool",
